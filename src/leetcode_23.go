14d898402860a29765f4be9010bea03745ce2aee
kevinlmll <smouse_hj@qq.com>
2019-11-12 11:51:31 +0800

    resolve p23

---
+++ src/leetcode_23.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 普通的merge sort方式，算法复杂度是O(mn)
+func mergeKLists1(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	var head *ListNode = nil
+	var tail *ListNode = nil
+	listIdx := make([]*ListNode, len(lists))
+	for i := range lists {
+		listIdx[i] = lists[i]
+	}
+
+	for {
+		nilNum := 0
+		var roundIdx int = 0
+		var roundNode *ListNode = nil
+		for i, ln := range listIdx {
+			if ln == nil {
+				nilNum += 1
+				continue
+			}
+			if roundNode == nil {
+				roundNode = ln
+				roundIdx = i
+			} else {
+				if ln.Val < roundNode.Val {
+					roundNode = ln
+					roundIdx = i
+				}
+			}
+		}
+		if nilNum == len(listIdx) {
+			break
+		}
+		listIdx[roundIdx] = roundNode.Next
+
+		if head == nil {
+			head = roundNode
+			tail = roundNode
+		} else {
+			tail.Next = roundNode
+			tail = roundNode
+		}
+	}
+	return head
+}
+
+// 已知链表是排序的情况下，可以做优化
+// 最优情况是O(m)，最差情况下是O(nm)，平均是介于两者中间
+func mergeKLists2(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	var head *ListNode = nil
+	var tail *ListNode = nil
+	minList := make([]*ListNode, 0)
+	for i := range lists {
+		if lists[i] != nil {
+			minList = append(minList, lists[i])
+		}
+	}
+	if minList == nil || len(minList) == 0 {
+		return nil
+	}
+
+	sort.Slice(minList, func(i, j int) bool {
+		return minList[i].Val < minList[j].Val
+	})
+
+	for i := 0; i < len(minList); {
+		//fmt.Printf("%d\n", i)
+		nextIdx := i + 1
+		if nextIdx >= len(minList) {
+			//没有下一级队列，把当前队列遍历完即可
+			if tail != nil {
+				tail.Next = minList[i]
+			} else {
+				tail = minList[i]
+				head = tail
+			}
+			break
+		}
+		if minList[i] == nil {
+			//当前队列已经为空了
+			i++
+			continue
+		}
+
+		//遍历当前队列，直到Val>minList[nextIdx].Val
+		node := minList[i]
+		for node != nil && node.Val <= minList[nextIdx].Val {
+			//fmt.Printf("current idx:%d, node:%+v\n", i, *node)
+			if head == nil {
+				tail = node
+				head = node
+			} else {
+				tail.Next = node
+				tail = node
+			}
+			node = node.Next
+		}
+		minList[i] = node
+		if node != nil {
+			j := i
+			for j < len(minList)-1 {
+				next := j + 1
+				if minList[j].Val > minList[next].Val {
+					tmp := minList[j]
+					minList[j] = minList[next]
+					minList[next] = tmp
+				}
+				j++
+			}
+		}
+	}
+	return head
+}
+
+func main() {
+	n11 := ListNode{1, nil}
+	n12 := ListNode{4, nil}
+	n13 := ListNode{5, nil}
+	n11.Next = &n12
+	n12.Next = &n13
+
+	n21 := ListNode{1, nil}
+	n22 := ListNode{3, nil}
+	n23 := ListNode{4, nil}
+	n21.Next = &n22
+	n22.Next = &n23
+
+	n31 := ListNode{2, nil}
+	n32 := ListNode{6, nil}
+	n31.Next = &n32
+
+	lists := make([]*ListNode, 0)
+	//lists = append(lists, &n11, &n21, &n31)
+	lists = append(lists, nil, nil)
+	fmt.Printf("%s\n", mergeKLists2(lists))
+}
